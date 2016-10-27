ab337ab13e006a86b50e7fe7947c70eee7a93416
Greg Murray <Gmurr@Gregs-MacBook-Pro.local>
2016-10-27 02:59:45 -0500

    Fox scraper added

--- DownloadPlayers.py
+++ DownloadPlayers.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python
 import csv
-import requests
-from bs4 import BeautifulSoup
 
 from Player import Player
 
@@ -24,33 +22,7 @@ def DownloadPlayers(fileName):
 			allEligiblePlayers[name] = newPlayer
 	return allEligiblePlayers
 
-#get Projected values from NFL site
-def getProjectedFromNFL(n):
-	ogUrl="http://fantasy.nfl.com/research/projections"
-	url = "http://fantasy.nfl.com/research/projections"
-	count = 0
-	nflPlayers = {}
-	while count < n:
-		r = requests.get(url)
-		soup = BeautifulSoup(r.content, "html.parser")
-		playerInfo = soup.find_all("tr")
-		for info in playerInfo:
-			name = ""
-			projected = -100.0
-			for tag in info:
-				player = tag.find_all("a", {"class": "playerName"})
-				if len(player) > 0:
-					name = player[0].text
-				pts = tag.find_all("span" ,{"class": "playerWeekProjectedPts"})
-				if len(pts) > 0:
-					projected = pts[0].text
-			if name != "" and projected != -100.0:
-				nflPlayers[name] = projected
-				print name, projected
-				count+=1
-		nextLink = soup.find_all("li", {"class": "next"})
-		url = ogUrl+nextLink[0].find_all("a")[0].get("href")
-	return nflPlayers
+
 
 
 
--- NFLProjectedPointsScraper.py
+++ NFLProjectedPointsScraper.py
@@ -3,26 +3,29 @@ import csv
 import requests
 from bs4 import BeautifulSoup
 
-#goes through the tags and finds the name and projected pts for the week
+
+global nflUrl, foxUrl
+nflUrl = "http://fantasy.nfl.com/research/projections"
+foxUrl = "http://www.foxsports.com/fantasy/football/commissioner/Research/Projections.aspx"
+
+#goes through the tags of playerInfo and finds the name and projected pts for the week
 def getNFLProjected(playerInfo, nflPlayers):
 	for info in playerInfo:
 		name = ""
 		projected = -1000.0
-		for tag in info:
-			player = tag.find_all("a", {"class": "playerName"})
-			if len(player) > 0:
-				name = player[0].text
-			pts = tag.find_all("span" ,{"class": "playerWeekProjectedPts"})
-			if len(pts) > 0:
-				projected = pts[0].text
+		player = info.find_all("a", {"class": "playerName"})
+		if len(player) > 0:
+			name = player[0].text
+		pts = info.find_all("span" ,{"class": "playerWeekProjectedPts"})
+		if len(pts) > 0:
+			projected = float(pts[0].text)
 		if name != "" and projected != -1000.0:
 			nflPlayers[name] = projected
+			print name, projected
 
 #get Projected values from NFL site
 def getProjectedFromNFL(n):
-	ogUrl="http://fantasy.nfl.com/research/projections"
-	url = "http://fantasy.nfl.com/research/projections"
-	count = 0
+	url = ""+nflUrl
 	nflPlayers = {}
 	while len(nflPlayers) < n:
 		r = requests.get(url)
@@ -30,7 +33,38 @@ def getProjectedFromNFL(n):
 		playerInfo = soup.find_all("tr")
 		getNFLProjected(playerInfo, nflPlayers)
 		nextLink = soup.find_all("li", {"class": "next"})
-		url = ogUrl+nextLink[0].find_all("a")[0].get("href")
+		try:
+			nextPage = nextLink[0].find_all("a")[0].get("href")
+		except:
+			print "Next page not found... Awkward"
+			break
+		url = nflUrl+nextPage
 	return nflPlayers
 
-getProjectedFromNFL(25)
+def getProjectedFromFox(n):
+	url =""+foxUrl
+	nflPlayers = {}
+	while len(nflPlayers) < n:
+		r = requests.get(url)
+		soup = BeautifulSoup(r.content, "html.parser")
+		playerInfo = soup.find_all("tr")
+		for info in playerInfo:
+			name = ""
+			pts = -1000.0
+			player = info.find_all("a", {"class": "wis_playerProfileLink"})
+			if len(player) > 0:
+				name = player[0].text
+			projected = info.find_all("td", {"class": "wis_col_highlight"})
+			if len(projected) > 0:
+				pts = float(projected[0].text)
+			if name != "" and pts != -1000.0:
+				nflPlayers[name] = pts
+				print name, pts
+		nextLink = soup.find_all("a", {"class": "wis_nextPageLink"})
+		try:
+			nextPage = nextLink[0].find_all("a")[0].get("href")
+		except:
+			print "Next page not found... Awkward"
+			break
+		url = foxUrl+nextPage
+	return nflPlayers
