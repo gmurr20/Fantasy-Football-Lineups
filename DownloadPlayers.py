833b5fae3bd37ae4a9c1af77dacb41d9bf21bafd
Greg Murray <Gmurr@Gregs-MacBook-Pro.local>
2016-10-27 02:11:35 -0500

    Started working on web scraper for nfl projections

--- DownloadPlayers.py
+++ DownloadPlayers.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python
-from Player import Player
 import csv
+import requests
+from bs4 import BeautifulSoup
 
-#Download Players from csv file and return an array of Players
+from Player import Player
+
+#Download Players from csv file and returns a dictionary of Players
 def DownloadPlayers(fileName):
-	allEligiblePlayers = []
+	allEligiblePlayers = {}
 	with open(fileName, 'r') as file:
 		fileReader = csv.reader(file)
 		skipFirst = True
@@ -18,7 +21,36 @@ def DownloadPlayers(fileName):
 			projected = float(row[4])
 			team = row[5]
 			newPlayer = Player(salary, name, team, position, projected)
-			newPlayer.printPlayer()
-			allEligiblePlayers.append(newPlayer)
+			allEligiblePlayers[name] = newPlayer
 	return allEligiblePlayers
+
+#get Projected values from NFL site
+def getProjectedFromNFL(n):
+	ogUrl="http://fantasy.nfl.com/research/projections"
+	url = "http://fantasy.nfl.com/research/projections"
+	count = 0
+	nflPlayers = {}
+	while count < n:
+		r = requests.get(url)
+		soup = BeautifulSoup(r.content, "html.parser")
+		playerInfo = soup.find_all("tr")
+		for info in playerInfo:
+			name = ""
+			projected = -100.0
+			for tag in info:
+				player = tag.find_all("a", {"class": "playerName"})
+				if len(player) > 0:
+					name = player[0].text
+				pts = tag.find_all("span" ,{"class": "playerWeekProjectedPts"})
+				if len(pts) > 0:
+					projected = pts[0].text
+			if name != "" and projected != -100.0:
+				nflPlayers[name] = projected
+				print name, projected
+				count+=1
+		nextLink = soup.find_all("li", {"class": "next"})
+		url = ogUrl+nextLink[0].find_all("a")[0].get("href")
+	return nflPlayers
+
+
 
